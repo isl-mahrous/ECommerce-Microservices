55c05e2f3eb0812e285f87c2d772aac999768c36
Islam Mahrous <66200462+isl-mahrous@users.noreply.github.com>
2022-06-18 15:10:41 +0200

    Order.Infrastructure, CQRS Implementaion, OrderSeed, Email Services ... etc

--- Services/Ordering/Ordering.API/Startup.cs
+++ Services/Ordering/Ordering.API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.Application;
+using Ordering.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +33,12 @@ namespace Ordering.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
+
+            // Add Application Dependencies using extension method
+            services.AddApplicationServices();
+
+            // Add Infrastructure Dependencies using extension method
+            services.AddInfrastructureServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
